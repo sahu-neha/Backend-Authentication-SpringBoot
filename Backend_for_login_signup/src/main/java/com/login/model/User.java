d380e246d3feca6a6603530162c462bebff0d077
Neha <nehasahu2227@gmail.com>
2023-07-07 03:15:42 +0530

    first draft

---
+++ Backend_for_login_signup/src/main/java/com/login/model/User.java
@@ -0,0 +1,42 @@
+package com.login.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotEmpty(message = "First name cannot be empty")
+	private String firstName;
+
+	@NotEmpty(message = "Last name cannot be empty")
+	private String lastName;
+
+	@NotEmpty(message = "Email cannot be empty")
+	@Email(message = "Invalid email format")
+	private String email;
+
+	@NotEmpty(message = "Password cannot be empty")
+	@Size(min = 6, message = "Password must be at least 6 characters long")
+	private String password;
+
+	@JsonIgnore
+	private String roles;
+}
